f2d9085883a6fc133082eab67938728ab70f3920
Vincent Liu <vincentliu362@gmail.com>
2024-09-13 00:07:20 -0500

    change inject name

--- src/entities/censustract.providers.ts
+++ src/entities/censustract.providers.ts
@@ -5,6 +5,6 @@ export const censustractProvider = [
   {
     provide: 'CENSUSTRACT_REPOSITORY',
     useFactory: (dataSource: DataSource) => dataSource.getRepository(CensusTract),
-    inject: ['DATA_SOURCE'],
+    inject: ['POSTGRES'],
   },
 ];
