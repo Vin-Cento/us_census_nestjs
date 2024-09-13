9f88bbed1d01b98b092dd853c9ee54e7b849ef0c
Vincent Liu <vincentliu362@gmail.com>
2024-09-13 00:07:32 -0500

    add rest of columns

--- src/entities/censustract.entity.ts
+++ src/entities/censustract.entity.ts
@@ -7,4 +7,37 @@ export class CensusTract {
 
   @Column('text')
   censuscode: string;
+
+  @Column('text')
+  statefp: string;
+
+  @Column('text')
+  countyfp: string;
+
+  @Column('text')
+  tractce: string;
+
+  @Column('text')
+  blkgrpce: string;
+
+  @Column('text')
+  affgeoid: string;
+
+  @Column('text')
+  geoid: string;
+
+  @Column('text')
+  lsad: string;
+
+  @Column('bigint')
+  aland: number;
+
+  @Column('bigint')
+  awater: number;
+
+  @Column('text')
+  state: string;
+
+  @Column('geometry', { spatialFeatureType: 'MultiPolygon', srid: 4269 })
+  geometry: string;
 }
